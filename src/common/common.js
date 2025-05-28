5705ccf6a0177dfc1cd38c85c4a8cc88297d5907
sylesh7 <sylesh.27it@licet.ac.in>
2025-05-28 11:38:42 +0530

    Fixed size

--- src/common/common.js
+++ src/common/common.js
@@ -1,7 +1,8 @@
 export var width = window.innerWidth;
 export var height = window.innerHeight;
 
-if (width >= 500) { //if this desktop
-  width  = 320;
-  height = 480;
-}
+window.addEventListener("resize", () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+});
+
